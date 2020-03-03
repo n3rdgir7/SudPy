65558568cd54f56a627439a0c3e00f8451a0c043
n3rdgir7 <vjkealiher@gmail.com>
2020-03-02 16:55:37 -0800

    second update

--- sud.py
+++ sud.py
@@ -40,6 +40,7 @@ for i in range(1, 6):     #check all 9 possible digits i    CHANGE ME BACK TO 1,
     for key,value in Counter(boxes[:, 0]).items():     #check rows for count of i, k is box, value is count
         if (value == 2):     #if count of i is 2, we need to find 3rd i in that row
             col = int(3 - sum(cols[np.argwhere(rows == key)]))
+            #print(col)
             for y in range((key*3),(key*3+3)):     #loop checks each of the 3 y rows in the box
                 if not np.array([i]) in puzzle[y, :]:     #if i is not in that row 
                     possible = 0
@@ -49,9 +50,9 @@ for i in range(1, 6):     #check all 9 possible digits i    CHANGE ME BACK TO 1,
                             possible += 1
                             print(i, "is in: ", y,x)
                         print (y, x, possible)
-                if possible == 1:
+                '''if possible == 1:
                     print("do it")
-                        #puzzle[y][x] = i
+                        #puzzle[y][x] = i'''
                     
 
 
